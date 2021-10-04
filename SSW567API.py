93fc16c8fa8e313f8db3fce57d313afafb424a54
Joan Marie Tubungbanua <joantubungbanua@gmail.com>
2021-10-03 20:30:16 -0400

    add test and travis file

--- SSW567API.py
+++ SSW567API.py
@@ -4,28 +4,31 @@ import json
 
 
 def getGithubRepoInfo(userID): 
-    query_url1 = f"https://api.github.com/users/{userID}/repos"
-    r = requests.get(query_url1)
-    names = r.json() 
-    repoList = []
-    for i in range(0,len(names)): 
-        repoList.append(names[i]['name'])
-    
-    for j in range(0,len(repoList)): 
-        query_url2 = f"https://api.github.com/repos/{userID}/{repoList[j]}/commits"
-        test = requests.get(query_url2)
-        commits = test.json()
-        total = 0
-        print("Repo Name: " + repoList[j])
-        for k in range(0,len(commits)):
-            total += 1
-        print("Commits: ") 
-        print (total)
-    
-  
-    
-
-
+    #checks to see if user entered an empty string 
+    if(userID == ""): 
+        return 'Username Missing'
+    #since Github username has a max of 39 characters, checks to see if what was entered actually works 
+    elif (len(userID)>=39): 
+        return 'Invalid Username'
+    #means user entered a valid username and will now list out associated repos and commits
+    else: 
+        query_url1 = f"https://api.github.com/users/{userID}/repos"
+        r = requests.get(query_url1)
+        names = r.json() 
+        repoList = []
+        for i in range(0,len(names)): 
+            repoList.append(names[i]['name'])
+        for j in range(0,len(repoList)): 
+            query_url2 = f"https://api.github.com/repos/{userID}/{repoList[j]}/commits"
+            test = requests.get(query_url2)
+            commits = test.json()
+            total = 0
+            print("Repo Name: " + repoList[j])
+            for k in range(0,len(commits)):
+                total += 1
+            print("Commits: ") 
+            print (total)
+        return("Succesfully Printed!")
 
        
 
---
+++ testGithubAPI.py
@@ -0,0 +1,22 @@
+import unittest
+
+from SSW567API import getGithubRepoInfo
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestAPI(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testUsernameInput(self): 
+        self.assertEqual(getGithubRepoInfo(""),'Username Missing')
+    def testUsernameLength(self): 
+        self.assertEqual(getGithubRepoInfo("sdafsdlakfjsdjasdflkmsdkflaksdfskladfklsjdaklflsadklfjlsajdkfjskadfsdklfjslafljsadfklsajdlkfjslkajdfljsaldjflksjdlfjsjdafkjsjdalfksajdfkljsadlkfjlksadjfljsadlkf"),'Invalid Username')
+    def testPrintSuccess(self): 
+        self.assertEqual(getGithubRepoInfo("joantubungbanua"),'Successfuly Printed!')
+    
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
+
